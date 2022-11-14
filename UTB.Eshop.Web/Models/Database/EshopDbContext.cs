21de3b92389a40dcdddcadfda9cd8234f9c25410
psc-utb <psc.utb@gmail.com>
2022-11-14 16:47:52 +0100

    ASP.NET Core Identity + Instalace NuGet package pro ASP.NET Core Identity ~ Změna dědičnosti pro EshopDbContext -> IdentityDbContext + Přidání entit User, Role a enum Roles ~ Konfigurace aplikace pro použití Identity (registrace a konfigurace ve Startup.cs) + Přidána migrace pro tabulky z Identity a pro inicializaci tabulek + Přidána inicializace databáze pro nové tabulky -> přidání rolí, uživatelů a přiřazení rolí pro konkrétní uživatele + Přidána Area Security s AccountControllerem (bez implementace pro HttpPost Login a Register) a s View pro Login a Registraci + Přidán styl login.css pro registrační a přihlašovací formuláře + Přidání LoginViewModelu a RegisterViewModelu + Přidána a nakonfigurována služba pro přihlašování a registraci + Přidání odkazů do _Layout pro nepřihlášeného a přihlášeného uživatele

--- UTB.Eshop.Web/Models/Database/EshopDbContext.cs
+++ UTB.Eshop.Web/Models/Database/EshopDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UTB.Eshop.Web.Models.Entities;
+using UTB.Eshop.Web.Models.Identity;
 
 namespace UTB.Eshop.Web.Models.Database
 {
-    public class EshopDbContext : DbContext
+    public class EshopDbContext : IdentityDbContext<User, Role, int>
     {
         public DbSet<CarouselSlide> CarouselSlides { get; set; }
 
@@ -19,8 +22,23 @@ namespace UTB.Eshop.Web.Models.Database
         {
             base.OnModelCreating(builder);
 
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(entity.GetTableName().Replace("AspNet", String.Empty));
+            }
+
             DatabaseInit databaseInit = new DatabaseInit();
             builder.Entity<CarouselSlide>().HasData(databaseInit.CreateCarouselSlides());
+
+            builder.Entity<Role>().HasData(databaseInit.CreateRoles());
+
+            (User admin, List<IdentityUserRole<int>> adminUserRoles) = databaseInit.CreateAdminWithRoles();
+            (User manager, List<IdentityUserRole<int>> managerUserRoles) = databaseInit.CreateManagerWithRoles();
+
+            builder.Entity<User>().HasData(admin, manager);
+            builder.Entity<IdentityUserRole<int>>().HasData(adminUserRoles);
+            builder.Entity<IdentityUserRole<int>>().HasData(managerUserRoles);
+
         }
     }
 }
